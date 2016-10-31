e05a777ae7f9dd731c0da383e9f9848cf00097f7
Baishi Wu <baishi@compass.com>
2016-10-30 22:44:34 -0400

    Hooking up the labels

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -9,15 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var currentTemp: UILabel!
+    @IBOutlet weak var currentSummary: UILabel!
+    @IBOutlet weak var weatherImage: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let url = NSURL(string: "https://api.forecast.io/forecast/d3250bf407f0579c8355cd39cdd4f9e1/40.7138,73.9630") {
+        if let url = NSURL(string: "https://api.forecast.io/forecast/d3250bf407f0579c8355cd39cdd4f9e1/40.7138,-73.9630") {
             if let data = NSData(contentsOf: url as URL) {
                 do {
                     let parsed = try JSONSerialization.jsonObject(with: data as Data, options: JSONSerialization.ReadingOptions.allowFragments) as! [String:AnyObject]
                     print(parsed["currently"]!["summary"])
+                    
+                    self.currentTemp.text = "\(parsed["currently"]!["temperature"]!!)"
+                    self.currentSummary.text = "\(parsed["currently"]!["summary"]!!)"
                 }
                 catch let error as NSError {
                     print("A JSON parsing error occurred, here are the details:\n \(error)")
@@ -33,4 +39,3 @@ class ViewController: UIViewController {
 
 
 }
-
